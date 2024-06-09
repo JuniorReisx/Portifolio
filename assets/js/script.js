1e024471b6625ae1c210ebf1a326749ecff51a02
Junior Reis <gilmaikjunior@gmail.com>
2024-06-08 21:36:16 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,110 @@
+
+import { projetos } from "./projetos.js";
+
+const navigation = document.querySelector("#navigation");
+const backToTopButton = document.querySelector("#backToTopButton");
+const toggle = document.querySelector("#sw-checkbox");
+const projectsSection = document.querySelector("#projects .wrapper");
+
+window.addEventListener("load", function begin() {
+  projetos(projectsSection);
+  
+});
+
+window.addEventListener("scroll", onScroll);
+onScroll();
+
+
+
+function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(projects);
+  activateMenuAtCurrentSection(knowledge);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");
+
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add("scroll");
+  } else {
+    navigation.classList.remove("scroll");
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+openMenu();
+function openMenu() {
+  const openBtns = document.querySelectorAll(".open");
+  openBtns.forEach((e) => {
+    e.addEventListener("click", () => {
+      document.body.classList.add("menu-expanded");
+    });
+  });
+}
+
+closeMenu();
+function closeMenu() {
+  const closeBtns = document.querySelectorAll(".close");
+  closeBtns.forEach((e) => {
+    e.addEventListener("click", () => {
+      document.body.classList.remove("menu-expanded");
+    });
+  });
+}
+
+ScrollReveal({
+  origin: "bottom",
+  distance: "50px",
+  duration: 1000,
+}).reveal(
+  `#home, 
+  #home img, 
+  #about, 
+  #about header, 
+  #about p,
+  #about img,
+  #projects,
+  #projects header,
+  #projects .card,
+  #knowledge,
+  #knowledg header,
+  #knowledg .card,
+  #contact,
+  #contact header`
+);
+
+toggle.addEventListener("change", () => {
+  document.body.classList.toggle("light-mode");
+});
